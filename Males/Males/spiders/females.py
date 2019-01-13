1b6b7e1c445bee60a4f61c9dcb2be00b06edc805
Kartic <krtckaushal@gmail.com>
2019-01-13 19:44:34 +0530

    Add files via upload

--- Males/Males/spiders/females.py
+++ Males/Males/spiders/females.py
@@ -16,21 +16,21 @@ class FemalesItem(scrapy.Item):
 class Females(scrapy.Spider):
     name = "females"
     allowed_domains = ["healthyceleb.com"]
-    start_urls = ('https://healthyceleb.com/category/statistics/sports-stars/female-sports-stars/page/5',)
+    start_urls = ('https://healthyceleb.com/category/statistics/sports-stars/female-sports-stars/page/1',)
     custom_settings = {
     # specifies exported fields and order
     'FEED_EXPORT_FIELDS': ["id", "Name", "Gender", "Height", "Weight", "url"],
   }
 
-    '''def parse(self, response):      # this fucntion crawls to different pages
-        for i in range(2,20):
+    def parse(self, response):      # this fucntion crawls to different pages
+        for i in range(2,8):
             temp = self.start_urls[0]
             nlink = temp.split()[0]
             newlink = nlink[:-1] + str(i),
             self.start_urls = self.start_urls + newlink
-            yield scrapy.Request(newlink[0], callback = self.Nparse)'''
+            yield scrapy.Request(newlink[0], callback = self.Nparse)
     
-    def parse (self, response):     # this function choose from 20 different profiles in a page to scrape data
+    def Nparse (self, response):     # this function choose from 20 different profiles in a page to scrape data
         links = response.xpath('//*[@class="td-ss-main-content"]//h3/a/@href').extract()
         print(links)
         for link in links:
@@ -43,12 +43,12 @@ class Females(scrapy.Spider):
         #print(item['id'])
         
         #item['Name'] = response.xpath('//*/strong[contains(text(),"Quick Info")]/text()').extract_first().replace("Quick Info", "")
-        item['Name'] = response.xpath('//*[@id="post-'+str(response.url.split("/")[-1])+'"]/div[1]/header/h1/text()').extract_first()
+        item['Name'] = response.xpath('//*[@id="post-'+str(response.url.split("/")[-1])+'"]/div[1]/header/h1/text()').extract_first().replace(" Height Weight Body Statistics","").replace(" Height, Weight, Age, Body Statistics","").replace("\xa0Height Weight Body Statistics","")
 
         item['Gender'] = response.xpath('//*[@id="td-outer-wrap"]/div[2]/div/div[1]/div/span[3]/a/text()').extract_first().split()[0]
         
         item['Height'] = response.xpath('//*[@class="td-post-content"]/p[contains(text(),"cm")]/text()').extract_first().split("or")[1].replace("cm", "")
-        
+
         item['Weight'] = response.xpath('//*[@class="td-post-content"]/p[contains(text(),"kg")]/text()').extract_first().split("or")[0].replace("kg", "")
         ##item['Weight'] = response.xpath('//*[@id="post-'+str(response.url.split("/")[-1])+'"]/div[3]/table/tbody/tr[2]/td[2]/text()').extract_first().replace("kg", "")
         #item['Weight'] = response.xpath('//*[@id="post-'+str(response.url.split("/")[-1])+'"]/div[3]/p[14]/text()').extract_first().split("or")[0].replace("kg", "")
