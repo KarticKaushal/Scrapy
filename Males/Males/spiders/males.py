86b952707efc5679822cfcdcb539c8172239728e
Kartic <krtckaushal@gmail.com>
2019-01-13 21:49:31 +0530

    Add files via upload

--- Males/Males/spiders/males.py
+++ Males/Males/spiders/males.py
@@ -12,34 +12,46 @@ class MalesItem(scrapy.Item):
     url = scrapy.Field()
     pass
 
-
 class Males(scrapy.Spider):
     name = "males"
     allowed_domains = ["healthyceleb.com"]
-    start_urls = ('https://healthyceleb.com/category/statistics/sports-stars/male-sports-stars/page/1', 'https://healthyceleb.com/category/statistics/sports-stars/male-sports-stars/page/2', 'https://healthyceleb.com/category/statistics/sports-stars/male-sports-stars/page/3', 'https://healthyceleb.com/category/statistics/sports-stars/male-sports-stars/page/4', 'https://healthyceleb.com/category/statistics/sports-stars/male-sports-stars/page/5',)
+    start_urls = ('https://healthyceleb.com/category/statistics/sports-stars/male-sports-stars/page/1',)
+    custom_settings = {
+    # specifies exported fields and order
+    'FEED_EXPORT_FIELDS': ["id", "Name", "Gender", "Height", "Weight", "url"],
+  }
 
 
-    def parse (self, response):
-        links = response.xpath('//*[@class="td-ss-main-content"]//h3/a/@href').extract()
+    def parse(self, response):      # this fucntion crawls to different pages
+        for i in range(2,8):
+            temp = self.start_urls[0]
+            nlink = temp.split()[0]
+            newlink = nlink[:-1] + str(i),
+            self.start_urls = self.start_urls + newlink
+            yield scrapy.Request(newlink[0], callback = self.Nparse)
+
+    def Nparse (self, response):    # this function choose from 20 different profiles in a page to scrape data
+        links = response.xpath('//*[@class="td-ss-main-content"]//h3/a/@href').extract() # links to diff profiles on page
         print(links)
         for link in links:
             abs_url = response.urljoin(link)
             yield scrapy.Request(abs_url, callback = self.parse_indetail)
 
+
     def parse_indetail(self, response):
         item = MalesItem()
         item['id'] = response.url.split("/")[-1]
-        print(item['id'])
-        w = item['id']
-        
+        #print(item['id'])
+
         item['Name'] = response.xpath('//*/strong[contains(text(),"Quick Info")]/text()').extract_first().replace("Quick Info", "")
         
-        item['Gender'] = response.xpath('//*[@id="td-outer-wrap"]/div[2]/div/div[1]/div/span[3]/a/text()').extract()
+        item['Gender'] = response.xpath('//*[@id="td-outer-wrap"]/div[2]/div/div[1]/div/span[3]/a/text()').extract_first().split()[0]
         
         item['Height'] = response.xpath('//*[@class="td-post-content"]/p[contains(text(),"cm")]/text()').extract_first().split("or")[1].replace("cm", "")
         
         #item['Weight'] = response.xpath('//*[@class="td-post-content"]/p[contains(text(),"kg")]/text()').extract_first().split("or")[0].replace("kg", "")
-        item['Weight'] = response.xpath('//*[@id="post-'+str(w)+'"]/div[3]/table/tbody/tr[2]/td[2]/text()').extract_first().replace("kg", "")
+        item['Weight'] = response.xpath('//*[@id="post-'+str(response.url.split("/")[-1])+'"]/div[3]/table/tbody/tr[2]/td[2]/text()').extract_first().replace("kg", "")
 
         item['url'] = response.url
+        
         yield item
